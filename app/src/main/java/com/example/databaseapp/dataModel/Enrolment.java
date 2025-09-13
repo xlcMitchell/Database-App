f8516e325fa077495ac6ee293f212a0d000c6754
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-09-13 12:48:32 +1200

    Enrolment class added

---
+++ app/src/main/java/com/example/databaseapp/dataModel/Enrolment.java
@@ -0,0 +1,53 @@
+package com.example.databaseapp.dataModel;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "enrolment", foreignKeys={
+        @ForeignKey(entity = Student.class,
+                parentColumns = "student_id",
+                childColumns = "student_id"),
+        @ForeignKey(
+                entity= Course.class,
+                parentColumns = "course_id",
+                childColumns = "course_id"
+        )
+
+})
+public class Enrolment {
+    @PrimaryKey
+    @ColumnInfo(name = "enrolment_id")
+    private int enrolmentId;
+    @ColumnInfo(name = "student_id")
+    private int studentId;
+    @ColumnInfo(name = "course_id")
+    private int courseId;
+    public Enrolment(){}
+
+    public void setEnrolmentId(int enrolmentId) {
+        this.enrolmentId = enrolmentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public int getEnrolmentId() {
+        return enrolmentId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+}
+
